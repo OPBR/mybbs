8621ddcaa9622fa1be3197ef8d28b5efe4569120
zhangy <openbraver@gmail.com>
2017-05-14 23:25:39 +0800

    修改后台操作失败响应实体类

--- src/main/java/org/zhangy/mybbs/dto/json/impl/FailedResponse.java
+++ src/main/java/org/zhangy/mybbs/dto/json/impl/FailedResponse.java
@@ -8,8 +8,13 @@ import org.zhangy.mybbs.dto.json.Response;
 public class FailedResponse implements Response {
     public static final String CODE_SUCCESS = "2";
     public static final String MSG_SUCCESS = "";
+    private String content;
 
     public FailedResponse() {
+    }
+
+    public FailedResponse(String content) {
+        this.content = content;
     }
 
     public String getCode() {
@@ -18,5 +23,13 @@ public class FailedResponse implements Response {
 
     public String getMsg() {
         return "";
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
