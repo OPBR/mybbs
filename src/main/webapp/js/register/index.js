e6745957659272bde9d1735fa86fe434041d39a3
zhangy <openbraver@gmail.com>
2017-05-15 10:55:22 +0800

    注册页面表达验证JS 文件修改

--- src/main/webapp/js/register/index.js
+++ src/main/webapp/js/register/index.js
@@ -1,32 +1,55 @@
 /**
  * Created by zy on 2017/5/14.
  */
+var zy = {};
+zy.GetLength = function(str) {
+    var realLength = 0, len = str.length, charCode = -1;
+    for (var i = 0; i < len; i++) {
+        charCode = str.charCodeAt(i);
+        if (charCode >= 0 && charCode <= 128) realLength += 1;
+        else realLength += 2;
+    }
+    return realLength;
+};
 $(function () {
-    $('#registerForm').validate({
-        rules: {
-            username: {
-                required: true,
-                minlength: 4
-            },
-            realName: "required",
-            password: {
-                required: true,
-                minlength: 5
-            }
-        },
-        message: {
-            username: {
-                required: "请输入用户名",
-                minlength: "最少为四位"
-            },
-            realName: "不为空",
-            password: {
-                required: "不能为空",
-                minlength: "最少为五位"
-            }
+    $(document).keydown(function (event) {
+        if (event.keyCode == 13){
+            $('#registerForm').submit();
         }
     });
-   $('#subBtn').click(function () {
+    $('input').on('keyup', function () {
+        $('#hint').attr("class", "alert alert-danger display-hide");
+        return false;
+    });
+
+    $('#registerForm').submit(function () {
+       $('#subBtn').click();
+       $("input").blur();
+       return false;
+   });
+    $('#subBtn').click(function () {
+        var idCard = /^\d{6}(18|19|20)?\d{2}(0[1-9]|1[12])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i;
+        var phone = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+        if (zy.GetLength($('#username').val()) < 4){
+           $('#hint').html('<span>用户名太短</span>');
+           $('#hint').attr("class", "alert alert-danger");
+           return false;
+       }
+       if ($('#realname').val().length == 0){
+           $('#hint').html('<span>请输入有效真实姓名</span>');
+           $('#hint').attr("class", "alert alert-danger");
+           return false;
+       }
+       if (idCard.test($('#realid').val()) == false){
+           $('#hint').html('<span>请输入有效身份证号</span>');
+           $('#hint').attr("class", "alert alert-danger");
+           return false;
+       };
+       if (phone.test($('#phone').val()) == false){
+           $('#hint').html('<span>请输入有效手机号</span>');
+           $('#hint').attr("class", "alert alert-danger");
+           return false;
+       };
        $.ajax({
            url:  "user/register",
            type: "POST",
