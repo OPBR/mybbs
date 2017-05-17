8e8e9a319a904429f07dc95a61ac3a3f57d8817d
zhangy <openbraver@gmail.com>
2017-05-17 09:05:04 +0800

    登录页实现记住密码功能

--- src/main/webapp/js/login/index.js
+++ src/main/webapp/js/login/index.js
@@ -2,8 +2,16 @@
  * Created by DELL on 2017/5/13.
  */
 $(function () {
+   $(document).ready(function () {
+       if ($.cookie("rmbUser") == "true") {
+           $("#ck_rmbUser").attr("checked", true);
+           $("#username").val($.cookie("username"));
+           $("#password").val($.cookie("password"));
+       }
+   });
    $(document).keydown(function (event) {
        if (event.keyCode == 13){
+          $('#subBtn').click();
           return false;
        }
    });
@@ -18,11 +26,27 @@ $(function () {
            dataType: 'JSON',
            success: function (data) {
                if (data.code == "1"){
+                   Save();
                    parent.location.href = "user/index";
                } else if (data.code == "2"){
+                   $('#hint').html('<span>用户名或密码错误</span>');
                    $('#hint').attr("class", "alert alert-danger");
                }
            }
        });
    });
 });
+function Save() {
+    if ($("#ck_rmbUser").is(':checked')) {
+        var str_username = $("#username").val();
+        var str_password = $("#password").val();
+        $.cookie("rmbUser", "true", { expires: 7 }); //存储一个带7天期限的cookie
+        $.cookie("username", str_username, { expires: 7 });
+        $.cookie("password", str_password, { expires: 7 });
+    }
+    else {
+        $.cookie("rmbUser", "false", { expire: -1 });
+        $.cookie("username", "", { expires: -1 });
+        $.cookie("password", "", { expires: -1 });
+    }
+};
