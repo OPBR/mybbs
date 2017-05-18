999ee19f6e3c6902964fedd604edccf125832aef
zhangy <openbraver@gmail.com>
2017-05-18 17:45:41 +0800

    添加更新内容所需要的JS文件

---
+++ src/main/webapp/js/content/updateContent.js
@@ -0,0 +1,26 @@
+/**
+ * Created by DELL on 2017/5/18.
+ */
+$(function () {
+    var ue = UE.getEditor('editor');
+    ue.addListener("ready", function () {
+        ue.setContent($('#hidInfo').val(), false);
+    });
+    $('#subBtn').on('click', function () {
+        $.ajax({
+            url: '/mybbs/content/updateSubmit',
+            type: 'POST',
+            data: {
+                'id': $('#hidContentId').val(),
+                'info': ue.getContent(),
+                'title': $('#title').val()
+            },
+            dataType: "JSON",
+            success: function (data) {
+                if ("1" === data.code){
+                    location.href = "/mybbs/user/index";
+                }
+            }
+        });
+    });
+});
